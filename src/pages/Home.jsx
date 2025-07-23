be05807982f164966761d431e766fe8e399a3372
Ashish-web-tech <akonwtf8@gmail.com>
2025-07-23 23:47:04 +0530

    fix bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Hero from '../components/HomePage/Hero/Hero'
 import MustVisit from '../components/HomePage/Must-Visit/MustVisit'
 import Explore from '../components/HomePage/Explore/Explore'
-import ReelSection from '../components/Homepage/Reels/ReelSection'
+import ReelSection from '../components/HomePage/Reels/ReelSection'
 import Map from '../components/HomePage/Map/Map'
 
 const Home = ({dark}) => (
